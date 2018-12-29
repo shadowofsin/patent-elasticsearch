20cf26d010f970b454c2058d40a105eadc6fcc96
lengjuanzhong <lengjuanzhong@daojia-inc.com>
2018-12-29 10:49:42 +0800

    首次提交

---
+++ src/main/java/com/shadow/patent/App.java
@@ -0,0 +1,16 @@
+package com.shadow.patent;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Hello world!
+ */
+@Slf4j
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/shadow/patent/AppTest.java
@@ -0,0 +1,20 @@
+package com.shadow.patent;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Unit test for simple App.
+ */
+@Slf4j
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        String testInfo = "Free flying flowers are like dreams";
+        log.info("The test info is :{}", testInfo);
+    }
+}
